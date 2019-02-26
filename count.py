cbab230a468598400362af1ad7e3dd4fb607b1b4
Aritra Sarkar <aritrasrkr1911@gmail.com>
2019-02-27 00:32:37 +0530

    Added completely different approach to increment which now shall also support step > 1

--- count.py
+++ count.py
@@ -1,21 +1,36 @@
+"""Houses the Count class."""
+
+
 class Count:
+    """Count in any base."""
+
     def __init__(self, base=10):
+        """Initialize counter with base."""
         self.base = base if base > 1 else 10
         self.stigid = list()  # Stores as OTH...
 
     def set_base(self, base):
-        """
-        WARNING: Resets counter list
-        """
+        """WARNING: Resets counter list."""
         self.base = base if base > 1 else 10
         self.reset()
 
-    def feed_zeroes(self, n):
-        """
-        Resets and loads n number of zeroes into counting list
-        """
-        self.stigid = [0] * n
+    def feed_zeroes(self, zeroes):
+        """Reset and load number of zeroes into counting list."""
+        self.stigid = [0] * zeroes
 
+    def increment_by(self, value, index=0):
+        """Increment by value."""
+        if value == 0:
+            return self.stigid[::-1]
+
+        if len(self.stigid) == index:
+            self.stigid.append(0)  # Code below takes care of the actual value
+
+        incr_value = self.stigid[index] + value
+        self.stigid[index] = incr_value % self.base
+        return self.increment_by(incr_value // self.base, index+1)
+
+    # TODO, Remove this as we got increment_by()
     def count(self):
         """
         Adds 1 (arithmetically) to existing
@@ -36,13 +51,9 @@ class Count:
         return self.stigid[::-1]  # invert and return
 
     def get_digits(self):
-        """
-        Returns existing list of digits
-        """
+        """Return existing list of digits."""
         return self.stigid[::-1]
 
     def reset(self):
-        """
-        Resets counting list to 0
-        """
+        """Reset count list to 0."""
         self.stigid = list()
